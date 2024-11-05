2eb1dc7dbb56c386e59ec0f023c5e4c55f130492
tomer <tomerbcool@gmail.com>
2024-11-05 13:21:20 +0200

    finished Q5

--- src/main/java/csYa_ronny/ArrayMerger.java
+++ src/main/java/csYa_ronny/ArrayMerger.java
@@ -2,9 +2,40 @@ package csYa_ronny;
 
 public class ArrayMerger {
     public static int[] mergeSortedArrays(int[] arr1, int[] arr2) {
-        // TODO: Implement the logic to merge two sorted arrays
-        return new int[] {};  // Return an empty array by default
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] mergedArray = new int[len1 + len2];
+        int i = 0, j = 0, k = 0;
+        while (i < len1 && j < len2)
+        {
+            if (arr1[i] <= arr2[j])
+            {
+                mergedArray[k] = arr1[i];
+                k++;
+                i++;
+            }
+            else
+            {
+                mergedArray[k] = arr2[j];
+                k++;
+                j++;
+            }
+        }
+        while (i < len1)
+        {
+            mergedArray[k] = arr1[i];
+            k++;
+            i++;
+        }
+        while (j < len2)
+        {
+            mergedArray[k] = arr2[j];
+            k++;
+            j++;
+        }
+        return mergedArray;
     }
+
 
     public static void main(String[] args) {
         // דוגמה לשימוש בשיטה
@@ -12,7 +43,6 @@ public class ArrayMerger {
         int[] array2 = {2, 4, 6};
         int[] result = mergeSortedArrays(array1, array2);
 
-        // הדפסת המערך הממוזג
         System.out.print("Merged array: ");
         for (int num : result) {
             System.out.print(num + " ");
